d40708fed8b3fa7712b08fd45384242f3be20a03
Jorge Freitas <jlcf04@gmail.com>
2024-02-19 09:49:09 -0300

    login e registro

--- core/security.js
+++ core/security.js
@@ -3,7 +3,8 @@ import bcrypt from 'bcryptjs';
 
 export async function verificarSenha(senha, hashSenha) {
   try {
-    return await bcrypt.compare(senha, hashSenha);
+    const hash = await bcrypt.compare(senha, hashSenha)
+    return hash;
   } catch (error) {
     console.error('Erro ao verificar senha:', error);
     throw new Error('Erro ao verificar senha');
@@ -14,7 +15,6 @@ export async function gerarHashSenha(senha) {
   try {
     const saltRounds = 10;
     const hash = await bcrypt.hash(senha, saltRounds);
-    console.log('hash', hash)
     return hash;
   } catch (error) {
     console.error('Erro ao gerar hash da senha:', error);
